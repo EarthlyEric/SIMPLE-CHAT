1662e77372d102417bc688c9fcee71c979ff336d
EarthlyEric-E4S <60834421+EarthlyEric@users.noreply.github.com>
2021-02-07 17:58:35 +0800

    NEWS

--- README.md
+++ README.md
@@ -1,18 +1,8 @@
-# 簡單聊天-zh_tw[![Run on Repl.it](https://repl.it/badge/github/EarthlyEric/SIMPLE-CHAT)](https://repl.it/github/EarthlyEric/SIMPLE-CHAT)
-# 功能
-* 1.登入頁面
-* 2.依賴程式庫已打包在程式碼發布包中
-* 3.登入和登出自動顯示在公共頻到道中
-* 4.自動註記訊息發出時間
-* 5.高度可自定義
-# 參考程式來源:
-* https://code.tutsplus.com/tutorials/how-to-create-a-simple-web-based-chat-application--net-5931
-* https://blog.xuite.net/mos1618/love/10035522
-# 感謝👍 https://gonative.io/
-# 程式碼來自https://home.error404studios.ml 製作
+# SIMPLE-CHAT[![Run on Repl.it](https://repl.it/badge/github/EarthlyEric/SIMPLE-CHAT)](https://repl.it/github/EarthlyEric/SIMPLE-CHAT)
 
+參考程式來源:https://code.tutsplus.com/tutorials/how-to-create-a-simple-web-based-chat-application--net-5931
 ![GITHUB]( 圖片網址 "圖片名稱")
-# Simple Chat-en_us[![Run on Repl.it](https://repl.it/badge/github/EarthlyEric/SIMPLE-CHAT)](https://repl.it/github/EarthlyEric/SIMPLE-CHAT)
 
-
+THANKS FOR 👍 https://gonative.io/
+by https://home.error404studios.ml
 
--- index.php
+++ index.php
@@ -10,47 +10,48 @@ if(isset($_POST['enter'])){
         echo '<span class="error">請輸入暱稱</span>';
     }
 }
-
+//
 if(isset($_POST['enter'])){
-    
+    //簡易加入訊息
     $login_message = "<div class='msgln'><span class='left-info'>使用者 <b class='user-name-left'>". $_SESSION['name'] ."</b> 加入聊天SKR.</span><br></div>";
     file_put_contents("log.html", $login_message, FILE_APPEND | LOCK_EX);
 }
 
 if(isset($_GET['logout'])){    
      
-    //Simple exit message
+    //簡易離開訊息
     $logout_message = "<div class='msgln'><span class='left-info'>使用者 <b class='user-name-come'>". $_SESSION['name'] ."</b> 離開了聊天QQ.</span><br></div>";
     file_put_contents("log.html", $logout_message, FILE_APPEND | LOCK_EX);
-     
+     //重定向使用者
     session_destroy();
-    header("Location: index.php"); //Redirect the user
+    header("Location: index.php"); 
 }
  
 function loginForm(){
-    echo
-    '<div id="loginform">
-    <p>E4S Web Simple Chat v.0.4</v></p>
-    <p>立即輸入暱稱 開始聊天吧</p>
-    <form action="index.php" method="post">
-      <label for="name">暱稱 &mdash;</label>
-      <input type="text" name="name" id="name" />
-      <input type="submit" name="enter" id="enter" value="開始" />
-    </form>
-    <a href="https://github.com/EarthlyEric/SIMPLE-CHAT"><img src="GitHub-Mark-32px.png"></a>
-  </div>
-  <embed src="SCP-Breach.mp3" autostart="true" hidden="true" loop="1" volume="25" width="0" height=0></embed>
-  ';
-}
- 
+    echo '
+    
+            <div id="loginform">
+            <p>E4S Web Simple Chat v.0.4</v></p>
+            <p>立即輸入暱稱 開始聊天吧</p>
+            <form action="index.php" method="post">
+              <label for="name">暱稱 &mdash;</label>
+              <input type="text" name="name" id="name" />
+              <input type="submit" name="enter" id="enter" value="開始" />
+            </form>
+            <a href="https://github.com/EarthlyEric/SIMPLE-CHAT"><img src="/img/Github/GitHub-Mark-32px.png"></a>
+            <a href="scp-music-player.html"><img src="/img/SCP/SCPICON32X.png"></a>
+            <embed src="audio\SCP-Breach.mp3" autostart="true" hidden="true" loop="1" volume="25" width="0" height="0" controls="volumelever"></embed>
+            </div>
+            ';
+} 
+
 ?>
  
 <!DOCTYPE html>
 <html lang="zh_tw">
     <head>
         <meta charset="utf-8" />
-        <link rel="Shortcut Icon" type="image/x-icon" href="E4S_ICON.ico" />
- 
+        <link rel="Shortcut Icon" type="image/x-icon" href="/icon/E4S_ICON.ico" />
         <title>E4S CHAT</title>
         <meta name="description" content="E4S CHAT" />
         <link rel="stylesheet" href="style.css" />
@@ -74,54 +75,15 @@ function loginForm(){
                 $contents = file_get_contents("log.html");          
                 echo $contents;
             }
-            ?>
+            ?> 
             </div>
- 
             <form name="message" action="">
                 <input name="usermsg" type="text" id="usermsg" />
                 <input name="submitmsg" type="submit" id="submitmsg" value="傳送" />
             </form>
-        </div>
-        <script type="text/javascript" src="jquery.min.js"></script>
-        <script type="text/javascript">
-            // jQuery Document
-            $(document).ready(function () {
-                $("#submitmsg").click(function () {
-                    var clientmsg = $("#usermsg").val();
-                    $.post("post.php", { text: clientmsg });
-                    $("#usermsg").val("");
-                    return false;
-                    echo 'call sound function';
-                });
-
-                function loadLog() {
-                    var oldscrollHeight = $("#chatbox")[0].scrollHeight - 20; //Scroll height before the request
- 
-                    $.ajax({
-                        url: "log.html",
-                        cache: false,
-                        success: function (html) {
-                            $("#chatbox").html(html); //Insert chat log into the #chatbox div
- 
-                            //Auto-scroll           
-                            var newscrollHeight = $("#chatbox")[0].scrollHeight - 20; //Scroll height after the request
-                            if(newscrollHeight > oldscrollHeight){
-                                $("#chatbox").animate({ scrollTop: newscrollHeight }, 'normal'); //Autoscroll to bottom of div
-                            }   
-                        }
-                    });
-                }
- 
-                setInterval (loadLog, 100);
- 
-                $("#exit").click(function () {
-                    var exit = confirm("你確定登出?");
-                    if (exit == true) {
-                    window.location = "index.php?logout=true";
-                    }
-                });
-            });
-        </script>
+            </div>
+        <script type="text/javascript" src="/js/jquery.min.js"></script>
+        <script type="text/javascript" src="/js/post.js"></script>
     </body>
 </html>
 <?php
